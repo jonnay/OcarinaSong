c82a5c786c3235a10b664b32e00055ffe643d9d9
Jack Crawford <jack.crawford.00@tased.edu.au>
2011-09-17 18:11:58 +1000

    Minor Bugfixes

--- plugin-OcarinaSong-1.0/src/main/java/me/jackcrawf3/ocarinasong/OcarinaSongBlockListener.java
+++ plugin-OcarinaSong-1.0/src/main/java/me/jackcrawf3/ocarinasong/OcarinaSongBlockListener.java
@@ -28,18 +28,20 @@ public Server server;
     @Override
     public void onSignChange(SignChangeEvent event) {
         Player player = event.getPlayer();
-        if (event.getLine(1).equals("[Awaken]")||event.getLine(1).equals("[awaken]")||event.getLine(1).toLowerCase().equals("§b[awaken]".toLowerCase())){
-            if (event.getPlayer().hasPermission("ocarina.awakening.sign")){
-                event.setLine(1, "§b[Awaken]");
-                event.getPlayer().sendMessage(ChatColor.AQUA+ "Created " + ChatColor.GRAY + "Awakening Detector" + ChatColor.AQUA + "!");
+        if (event.getBlock().getType()==Material.WALL_SIGN){
+            if (event.getLine(1).equals("[Awaken]")||event.getLine(1).equals("[awaken]")||event.getLine(1).toLowerCase().equals("§b[awaken]".toLowerCase())){
+                if (event.getPlayer().hasPermission("ocarina.awakening.sign")){
+                    event.setLine(1, "§b[Awaken]");
+                    event.getPlayer().sendMessage(ChatColor.AQUA+ "Created " + ChatColor.GRAY + "Awakening Detector" + ChatColor.AQUA + "!");
+                }
+                else{
+                    event.getPlayer().sendMessage(ChatColor.BLUE + "You don't understand ancient Magicks enough!");
+                    event.getPlayer().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.SIGN,1));
+                    event.getBlock().setType(Material.AIR);
+                    event.setCancelled(true);
+                }
+
             }
-            else{
-                event.getPlayer().sendMessage(ChatColor.BLUE + "You don't understand ancient Magicks enough!");
-                event.getPlayer().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.SIGN,1));
-                event.getBlock().setType(Material.AIR);
-                event.setCancelled(true);
-            }
-        
         }
         return;
     }
